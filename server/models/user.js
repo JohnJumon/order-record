d6b038f7b848741693e036ea3417403a21b7f1cc
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-29 13:55:03 +0700

    edit user

--- server/models/user.js
+++ server/models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema)
 
+/** 
 const staticUser = {
     username: process.env.STATIC_USERNAME,
     password: bcrypt.hashSync(process.env.STATIC_PASSWORD, 8),
@@ -17,5 +18,6 @@ const staticUser = {
 User.create(staticUser)
     .then(() => console.log("Static user created successfully"))
     .catch((error) => console.error("Error creating static user:", error));
+**/
 
 module.exports = User;
