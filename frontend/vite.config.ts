27e2d8b383f7beb27b46581fd0cf1f1ed672874a
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-29 17:35:32 +0700

    reset

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/frontend/', // Specify the base path for deployment
-  build: {
-    outDir: 'dist', // Specify the output directory for the build
-    assetsDir: 'assets', // Specify the assets directory for the build
-    sourcemap: true, // Enable source maps for debugging
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
+  plugins: [react()]
 })
