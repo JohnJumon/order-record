208b4539b748471e332974735a7f751a92ea1548
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-29 12:56:46 +0700

    add baseUrl to authStore

--- frontend/src/stores/authStore.tsx
+++ frontend/src/stores/authStore.tsx
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
+axios.defaults.baseURL = process.env.BASE_URL;
+axios.defaults.withCredentials = true;
 
 interface LoginForm {
     username: string;
