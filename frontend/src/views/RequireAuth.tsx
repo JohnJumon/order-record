034029ab90e8fdaa51523a7aa7e56152e1e16b01
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-29 14:43:09 +0700

    edit view

--- frontend/src/views/RequireAuth.tsx
+++ frontend/src/views/RequireAuth.tsx
@@ -24,7 +24,7 @@ export default function RequireAuth(props: Props) {
                 alignItems="center"
                 justifyContent="center"
                 mx="auto"
-                height="50vh"
+                height="60vh"
             >
                 <img src={noAccess} alt="noAccess" style={{ width: "50vw", marginBottom: "8px" }} />
                 <Typography variant="h6" gutterBottom style={{ textTransform: "uppercase", marginBottom: "0px", paddingBottom: "0px", textAlign:"center"}}>
