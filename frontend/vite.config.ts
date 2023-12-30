589ee0eee6040ea889989b323b3e827de584e622
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-30 14:00:37 +0700

    edit config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_BASE_URL: process.env.VITE_BASE_URL,
-    VITE_BUCKET_NAME: process.env.VITE_BUCKET_NAME,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      VITE_BASE_URL: JSON.stringify(env.VITE_BASE_URL),
+      VITE_BUCKET_NAME: JSON.stringify(env.VITE_BUCKET_NAME),
+    },
+  }
 })
