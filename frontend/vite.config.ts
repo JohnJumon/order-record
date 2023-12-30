cbc0369e26ccaac2401687295e5634810f38874c
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-30 13:53:41 +0700

    edit config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+    VITE_BASE_URL: process.env.VITE_BASE_URL,
+    VITE_BUCKET_NAME: process.env.VITE_BUCKET_NAME,
+  },
 })
