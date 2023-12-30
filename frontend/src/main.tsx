31bd8dd7811dc29486a3716106f37f49b68e26b0
JohnJumon <jhonsonrouslie66@gmail.com>
2023-12-30 13:43:05 +0700

    edit cors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ import ViewProductPage from './views/ViewProductPage.tsx'
 import ViewCustomerPage from './views/ViewCustomerPage.tsx'
 import LoginPage from './views/LoginPage.tsx'
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 const upload = multer()
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [process.env.ORIGIN],
   methods: ["GET", "POST", "PUT"],
   credentials: true,
 }));
